d362cdec4a407c7257db501320c78997b1b36136
Lee Robinson <lrobinson2011@gmail.com>
2024-08-10 20:57:26 -0500

    Fix search key

--- components/search.tsx
+++ components/search.tsx
@@ -31,7 +31,6 @@ export function Search({ query }: { query: string }) {
       <SearchIcon className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
       <Input
         onChange={handleInputChange}
-        key={query}
         type="text"
         name="q"
         id="search"
