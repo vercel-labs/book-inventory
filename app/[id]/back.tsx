00c25b3afc2260557d5dd7504e6c56444ed8540c
Howard Halim <howardhalim@gmail.com>
2024-08-12 12:19:17 -0400

    Fix back button

--- app/[id]/back.tsx
+++ app/[id]/back.tsx
@@ -5,9 +5,17 @@ import { useRouter } from 'next/navigation';
 export default function BackButton() {
   let router = useRouter();
 
+  function handleClick() {
+    if (typeof window !== "undefined" && window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  }
+
   return (
     <button
-      onClick={() => router.back()}
+      onClick={handleClick}
       className="p-3 mb-8 mr-auto rounded hover:bg-gray-100"
     >
       ← Back to all books
