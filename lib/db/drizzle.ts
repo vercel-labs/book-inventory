03527ddfc40049f1a68e01199b934618f389dfd3
Lee Robinson <me@leerob.io>
2024-09-02 09:36:54 -0500

    Update drizzle.ts

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.NEW_URL) {
+if (!process.env.POSTGRES_URL) {
   throw new Error('POSTGRES_URL environment variable is not set');
 }
 
 // TODO: Change back when ready
-export const sql = neon(process.env.NEW_URL);
+export const sql = neon(process.env.POSTGRES_URL);
 export const db = drizzle(sql);
